373fc7f93da00e1c5a240e76f1ef691c4a1eb814
bury-pink <97723662+bury-pink@users.noreply.github.com>
2025-02-22 15:52:11 +1300

    ugh

--- client/src/main.js
+++ client/src/main.js
@@ -3,11 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-axios.defaults.baseURL = process.env.MY_NODE_URL;
+axios.defaults.baseURL = process.env.VUE_APP_MY_NODE_URL;
 axios.defaults.withCredentials = true;
 
 const app = createApp(App);
